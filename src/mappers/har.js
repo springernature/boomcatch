652353afc1b266c61270f88ea989109b7301a97d
Phil Booth <pmbooth@gmail.com>
2014-04-04 14:43:53 +0100

    Add skeleton module for har mapper.

---
+++ src/mappers/har.js
@@ -0,0 +1,35 @@
+// Copyright © 2014 Nature Publishing Group
+//
+// This file is part of boomcatch.
+//
+// Boomcatch is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Boomcatch is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with boomcatch. If not, see <http://www.gnu.org/licenses/>.
+
+/*globals require, module */
+
+'use strict';
+
+var check = require('check-types'),
+    metrics = require('../metrics');
+
+module.exports = {
+    initialise: function (options) {
+        return map;
+    },
+    separator: '\n'
+};
+
+function map (data, referer) {
+    return '';
+}
+
