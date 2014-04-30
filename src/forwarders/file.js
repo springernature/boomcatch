d2948184494a73d07fb155c84e684e66437f3351
Phil Booth <pmbooth@gmail.com>
2014-04-30 16:14:02 +0100

    First cut file forwarder.

---
+++ src/forwarders/file.js
@@ -0,0 +1,40 @@
+// Copyright © 2014 Nature Publishing Group
+//
+// This file is part of boomcatch.
+//
+// Boomcatch is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Boomcatch is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with boomcatch. If not, see <http://www.gnu.org/licenses/>.
+
+/*globals require, exports */
+
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    check = require('check-types');
+
+exports.initialise = function (options) {
+    return send.bind(null, options.fwdDir);
+};
+
+function send (directory, data, separator, callback) {
+    fs.writeFile(
+        path.resolve('boomcatch-' + Date.now() + '.json', directory),
+        data,
+        { mode: 420 },
+        function (error) {
+            callback(error, data.length);
+        }
+    );
+}
+
