a0dbe59d4d8cb0d53eb46c04517eaeb4d1a84e5f
Phil Booth <pmbooth@gmail.com>
2014-12-15 16:56:21 +0000

    Fix lint error.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with boomcatch. If not, see <http://www.gnu.org/licenses/>.
 
-/*globals require, exports, process */
+/*globals require, exports, process, setTimeout */
 
 'use strict';
 
